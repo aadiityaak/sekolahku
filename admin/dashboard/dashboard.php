1a2ef49fef4682a6a77fc20660dbae74dee28d19
aadiityaak <aadiityaak@gmail.com>
2022-09-04 16:55:50 +0700

    add chart js mania

--- admin/dashboard/dashboard.php
+++ admin/dashboard/dashboard.php
@@ -45,7 +45,7 @@ function sekolahku_view_dashboard() {
                     </div>
                 </div>
                 <div class="col-md-3">
-                <div class="card border-0 bg-gradient-1 text-white">
+                    <div class="card border-0 bg-gradient-1 text-white">
                         <div class="p-2">
                             <h5 class="card-title">SMP</h5>
                             <p class="card-text">234</p>
@@ -53,10 +53,61 @@ function sekolahku_view_dashboard() {
                     </div>
                 </div>
                 <div class="col-md-3">
-                <div class="card border-0 bg-gradient-1 text-white">
+                    <div class="card border-0 bg-gradient-1 text-white">
                         <div class="p-2">
                             <h5 class="card-title">PONDOK</h5>
                             <p class="card-text">76</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-9">
+                    <canvas id="spp-sd" width="835" height="370"></canvas>
+                </div>
+                <div class="col-md-3">
+                    <div class="card border-0 bg-gradient-1 text-white">
+                        <div class="p-2">
+                            <h5 class="card-title">PONDOK</h5>
+                            <p class="card-text">76</p>
+                        </div>
+                    </div>
+                    <div class="card border-0 bg-gradient-1 text-white">
+                        <div class="p-2">
+                            <h5 class="card-title">PONDOK</h5>
+                            <p class="card-text">76</p>
+                        </div>
+                    </div>
+                    <div class="card border-0 bg-gradient-1 text-white">
+                        <div class="p-2">
+                            <h5 class="card-title">PONDOK</h5>
+                            <p class="card-text">76</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card border-0 bg-light text-dark">
+                        <div class="p-2">
+                            <h5 class="card-title">SPP Terlambat</h5>
+                            <p class="card-text">50</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                <div class="card border-0 bg-light text-dark">
+                        <div class="p-2">
+                            <h5 class="card-title">Uang Makan Terlambat</h5>
+                            <p class="card-text">50</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                <div class="card border-0 bg-light text-dark">
+                        <div class="p-2">
+                            <h5 class="card-title">List Tunggakan Tahun ini</h5>
+                            <p class="card-text">50</p>
                         </div>
                     </div>
                 </div>
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,13 +11,17 @@ defined( 'ABSPATH' ) || exit;
 function sekolahku_admin_style() {
     $page = isset($_GET['page']) ? $_GET['page'] : '';
 
+    wp_enqueue_script( 'jquery' );
+
     if($page=='dashboard-sekolah') {
         wp_enqueue_style('bootstrap-5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css');
         wp_enqueue_style('admin-bootstrap', SEKOLAHKU_URL.'asset/css/custom.css');
+
+        wp_enqueue_script( 'chart-js', 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js', array( 'jquery' ) );
     }
     wp_enqueue_style('my-admin-theme', SEKOLAHKU_URL.'asset/css/wp-admin.css');
 
-    wp_enqueue_script( 'jquery' );
+    
     wp_enqueue_script( 'jquery-csv', SEKOLAHKU_URL. 'asset/js/jquery.csv.min.js', array( 'jquery' ) );
     wp_enqueue_script( 'jquery-highlight', SEKOLAHKU_URL. 'asset/js/highlight.min.js', array( 'jquery' ) );
     wp_enqueue_script( 'custom-script', SEKOLAHKU_URL. 'asset/js/script.js', array( 'jquery' ) );
